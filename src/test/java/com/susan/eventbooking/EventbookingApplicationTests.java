cd004a04e09eee6b0e23647b2880703de8213fae
Susan Padath <susanpadath@Susans-MacBook-Air.local>
2026-03-02 21:22:33 +0900

    Initial commit

---
+++ src/test/java/com/susan/eventbooking/EventbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.susan.eventbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
